3299cb649ba5e036fdab5b14c4a2df546202a39d
LangLangGaming <vbt2050@gmail.com>
2026-02-04 17:05:33 +0800

    fix: fix deployment error from last commit

---
+++ src/assets/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/Loading.css';
+
+const Loading = () => (
+  <div className="loading-screen">
+    <div className="spinner" />
+  </div>
+);
+
+export default Loading;
