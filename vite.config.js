bb6403357269fd967dd1ba09f3c310d802c9ff5b
langlanggaming <vbt2050@gmail.com>
2026-02-18 16:06:31 +0800

    fix: fix syntax errors in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
 import { defineConfig } from 'vite'
 import tailwindcss from 'tailwindcss'
 import react from '@vitejs/plugin-react'
@@ -15,4 +6,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
->>>>>>> f644e1db94e6dc260bd97c32ab2c0cdf0125c00d
